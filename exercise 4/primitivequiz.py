75227ffc51567dafc2e8fd88ecdab2612f649fe2
Anagha <anaghaammurajeev@gmail.com>
2024-11-22 22:29:53 +0400

    primitivequiz.py
    this is a simple program that asks the user a question,
    evaluates their answer, and provides feedback. In this specific code, the user enters the name of the capital of France. If the answer is correct it will display that it is right. If not, it will say that it is incorrect.

---
+++ exercise 4/primitivequiz.py
@@ -0,0 +1,4 @@
+quiz_country=input("name the capital of France:") #question asked to user
+if quiz_country.strip().lower()=="Paris": print("that is the right answer") #to check if the answer is right or wrong
+else:
+    print("that is the wrong answer")
