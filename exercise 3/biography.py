5b24a87608d60c96c358af6029a07323eee08e1b
Anagha <anaghaammurajeev@gmail.com>
2024-11-22 22:18:08 +0400

    Create biography.py
    this code shows the information of a particular user such as her name, age and hometown in a dictionary.

---
+++ exercise 3/biography.py
@@ -0,0 +1,3 @@
+user_information={"name of user":"anagha akhila",
+"hometown of user": "kerala", "age": 18}
+print(f"Name: {user_information['name of user']}n\nHometown: {user_information['hometown of user']}\nAge: {user_information['age']}")
